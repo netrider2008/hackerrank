525f454161dce4d15175e6913e0b6bce965eff2c
Netrider <netrider2008@gmail.com>
2019-08-03 01:40:03 +0530

    submit
    submit

--- featurebranch/src/com/netrider/problems/hackerrank/misc/challenges/SolveMeFirstSolution.java
+++ featurebranch/src/com/netrider/problems/hackerrank/misc/challenges/SolveMeFirstSolution.java
@@ -3,26 +3,23 @@
  */
 package com.netrider.problems.hackerrank.misc.challenges;
 
-
 import java.util.Scanner;
-
 
 public class SolveMeFirstSolution {
 
+	static int solveMeFirst(int a, int b) {
+		return a + b;
 
-    static int solveMeFirst(int a, int b) {
-      	 return a+b;  
+	}
 
-   }
-
- public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a;
-        a = in.nextInt();
-        int b;
-        b = in.nextInt();
-        int sum;
-        sum = solveMeFirst(a, b);
-        System.out.println(sum);
-   }
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a;
+		a = in.nextInt();
+		int b;
+		b = in.nextInt();
+		int sum;
+		sum = solveMeFirst(a, b);
+		System.out.println(sum);
+	}
 }
